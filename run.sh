c145c27899ffe753c25129f97c3a4dcd13ee8dc4
poo <347547721@qq.com>
2017-05-11 20:39:43 +0800

    Add benchmarks.

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 mkdir -p traces/
 #../../../pin -t obj-intel64/membuffer.so -emit 1 -o traces/ls.trace -- /bin/ls
-../../../pin -t obj-intel64/membuffer.so -emit 1 -o traces/simple_pthread.trace -- test_progs/simple_pthread/simple_pthread
+#~ ../../../pin -t obj-intel64/membuffer.so -emit 1 -o traces/simple_pthread.trace -- test_progs/simple_pthread/simple_pthread
+../../../pin -t obj-intel64/membuffer.so -emit 1 -o traces/blackscholes.trace -- benchmarks/blackscholes/blackscholes 2 ../MemTrace/benchmarks/blackscholes/in_4K.txt ../MemTrace/benchmarks/blackscholes/prices.txt
